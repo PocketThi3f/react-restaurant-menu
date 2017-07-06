268dff4c2a02877a856f0655095184578141b1e8
PocketThi3f <krslai@yahoo.com>
2017-07-06 16:30:45 -0700

    adding reactive markup

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getFunName } from "../helpers.js"
 
 class StorePicker extends React.Component {
 	render() {
@@ -6,8 +7,9 @@ class StorePicker extends React.Component {
 			<form className="store-selector">
 				{/* Text goes inside input */} 
 				<h2>Please enter store name</h2>
-				<input type="text" required placeholder="Store Name" />
-				<button type="submit">Visit Store!</button>
+				<input type="text" required placeholder="Store Name" 
+				  defaultValue={getFunName()} />
+				<button type="submit">Visit Store →</button>
 			</form>
 		)
 	}
