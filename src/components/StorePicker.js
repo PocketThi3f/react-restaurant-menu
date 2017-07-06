916c21fa78771e2dba10142a4c80ff36b78d408c
PocketThi3f <krslai@yahoo.com>
2017-07-06 16:58:29 -0700

    adding react router methods

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,17 +2,34 @@ import React from "react";
 import { getFunName } from "../helpers.js"
 
 class StorePicker extends React.Component {
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+	goToStore(event) {
+		event.preventDefault();
+		console.log("change url");
+		// first grab text from box
+		const storeId = this.storeInput.value;
+		console.log(`Going to ${storeId}`);
+		// then display text and transition from / to /store:storeId
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
 				{/* Text goes inside input */} 
-				<h2>Please enter store name</h2>
+				<h2>Please Enter Store Name</h2>
 				<input type="text" required placeholder="Store Name" 
-				  defaultValue={getFunName()} />
+				  defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
 				<button type="submit">Visit Store →</button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
